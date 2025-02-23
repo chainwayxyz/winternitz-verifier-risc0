6d6bba9507ad00af2f18de3d92d7585e27b57652
aoengin <ahmetoguzengin@hotmail.com>
2025-02-23 18:28:52 +0300

    refactor: break code into separate files

--- winternitz/guest/src/lib.rs
+++ winternitz/guest/src/lib.rs
@@ -1,110 +1,19 @@
-use ark_bn254::{Bn254, Fr};
-use ark_groth16::PreparedVerifyingKey;
-use ark_serialize::CanonicalDeserialize;
 use bitcoin::hashes::Hash;
-use constants::{
-    A0_ARK, A1_ARK, ASSUMPTIONS, BN_254_CONTROL_ID_ARK, CLAIM_TAG, INPUT, OUTPUT_TAG, POST_STATE,
-    PREPARED_VK, PRE_STATE,
-};
-use hex::ToHex;
-use lc_proof::{lc_proof_verifier, verify_storage_proofs};
+use groth16::CircuitGroth16WithTotalWork;
+use storage_proof::verify_storage_proofs;
+use lc_proof::lc_proof_verifier;
 use risc0_zkvm::guest::env;
-use sha2::{Digest, Sha256};
 use std::str::FromStr;
 use winternitz_core::utils::hash160;
 use winternitz_core::winternitz::{
     verify_winternitz_signature, WinternitzCircuitInput, WinternitzCircuitOutput, WinternitzHandler
 };
 use winternitz_core::zkvm::ZkvmGuest;
-use winternitz_core::{groth16::CircuitGroth16Proof, utils::to_decimal};
+use winternitz_core::groth16::CircuitGroth16Proof;
 mod constants;
 mod lc_proof;
-
-pub fn create_output_digest(total_work: &[u8; 16]) -> [u8; 32] {
-    let total_work_digest: [u8; 32] = Sha256::digest(total_work).into();
-    let len_output: [u8; 2] = hex::decode("0200").unwrap().try_into().unwrap();
-
-    let output_pre_digest: [u8; 98] = [
-        &OUTPUT_TAG,
-        &total_work_digest[..],
-        &ASSUMPTIONS[..],
-        &len_output[..],
-    ]
-    .concat()
-    .try_into()
-    .expect("slice has correct length");
-
-    Sha256::digest(output_pre_digest).into()
-}
-
-pub fn create_claim_digest(output_digest: &[u8; 32]) -> [u8; 32] {
-    let data: [u8; 8] = [0; 8];
-
-    let claim_len: [u8; 2] = [4, 0];
-
-    let concatenated = [
-        &CLAIM_TAG,
-        &INPUT,
-        &PRE_STATE,
-        &POST_STATE,
-        output_digest,
-        &data[..],
-        &claim_len,
-    ]
-    .concat();
-
-    let mut claim_digest = Sha256::digest(concatenated);
-    claim_digest.reverse();
-
-    claim_digest.into()
-}
-pub struct CircuitGroth16WithTotalWork {
-    groth16_seal: CircuitGroth16Proof,
-    total_work: [u8; 16],
-}
-
-impl CircuitGroth16WithTotalWork {
-    pub fn new(
-        groth16_seal: CircuitGroth16Proof,
-        total_work: [u8; 16],
-    ) -> CircuitGroth16WithTotalWork {
-        CircuitGroth16WithTotalWork {
-            groth16_seal,
-            total_work,
-        }
-    }
-
-    pub fn verify(&self) -> bool {
-        let ark_proof = self.groth16_seal.into();
-        let start = env::cycle_count();
-        let prepared_vk: PreparedVerifyingKey<ark_ec::bn::Bn<ark_bn254::Config>> =
-            CanonicalDeserialize::deserialize_uncompressed(PREPARED_VK).unwrap();
-        let end = env::cycle_count();
-        println!("PVK: {}", end - start);
-        let start = env::cycle_count();
-
-        let output_digest = create_output_digest(&self.total_work);
-
-        let claim_digest: [u8; 32] = create_claim_digest(&output_digest);
-
-        let claim_digest_hex: String = claim_digest.encode_hex();
-        let c0_str = &claim_digest_hex[32..64];
-        let c1_str = &claim_digest_hex[0..32];
-
-        let c0_dec = to_decimal(c0_str).unwrap();
-        let c1_dec = to_decimal(c1_str).unwrap();
-
-        let c0 = Fr::from_str(&c0_dec).unwrap();
-        let c1 = Fr::from_str(&c1_dec).unwrap();
-
-        let public_inputs = vec![A0_ARK, A1_ARK, c0, c1, BN_254_CONTROL_ID_ARK];
-
-        let end = env::cycle_count();
-        println!("PPI: {}", end - start);
-        ark_groth16::Groth16::<Bn254>::verify_proof(&prepared_vk, &ark_proof, &public_inputs)
-            .unwrap()
-    }
-}
+mod storage_proof;
+mod groth16;
 
 pub fn verify_winternitz_and_groth16(input: &WinternitzHandler) -> bool {
     let start = env::cycle_count();
@@ -190,13 +99,18 @@ pub fn winternitz_circuit(guest: &impl ZkvmGuest) {
 
     // Light client proof verification
     let state_root = lc_proof_verifier(input.lcp.clone());
+
+    // Storage proof verification for deposit tx index and withdrawal outpoint
     let user_wd_outpoint_str= verify_storage_proofs(&input.sp, state_root);
+
     let user_wd_outpoint = num_bigint::BigUint::from_str(&user_wd_outpoint_str).unwrap();
     let user_wd_txid = bitcoin::Txid::from_byte_array(user_wd_outpoint.to_bytes_be().as_slice().try_into().unwrap());
     assert_eq!(user_wd_txid, input.payout_spv.transaction.input[0].previous_output.txid);
-    println!("{:?}", input.payout_spv.transaction.output);
+    println!("Payout transaction output: {:?}", input.payout_spv.transaction.output);
+
     let last_output = input.payout_spv.transaction.output.last().unwrap();
     let last_output_script = last_output.script_pubkey.to_bytes();
+
     assert!(last_output_script[0] == 0x6a);
     let len = last_output_script[1];
     let operator_id  = last_output_script[2..(2 + len as usize)].to_vec();
@@ -211,5 +125,4 @@ pub fn winternitz_circuit(guest: &impl ZkvmGuest) {
     });
     let end = env::cycle_count();
     println!("WNT: {}", end - start);
-
 }
